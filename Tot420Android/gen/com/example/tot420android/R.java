a9119c71453c98b3815f4d8a28d1a8f6f4f1ecfb
Karl Ingi <karlie@keilir.net>
2013-06-05 22:12:10 +0000

    tengi og ýtlitsbreytingar

--- Tot420Android/gen/com/example/tot420android/R.java
+++ Tot420Android/gen/com/example/tot420android/R.java
@@ -14,16 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout=0x7f070000;
-        public static final int button1=0x7f070002;
-        public static final int menu_settings=0x7f070009;
-        public static final int scrollView1=0x7f070007;
+        public static final int connect=0x7f070000;
+        public static final int menu_settings=0x7f070008;
         public static final int seekBar1=0x7f070003;
-        public static final int seekBar2=0x7f070004;
-        public static final int seekBar3=0x7f070005;
-        public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070006;
-        public static final int textView3=0x7f070008;
+        public static final int seekBar2=0x7f070005;
+        public static final int seekbar=0x7f070001;
+        public static final int seekbarvalue=0x7f070002;
+        public static final int textView2=0x7f070004;
+        public static final int textView4=0x7f070006;
+        public static final int textlog=0x7f070007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Tot420Android/src/com/example/tot420android/MainActivity.java
+++ Tot420Android/src/com/example/tot420android/MainActivity.java
@@ -4,244 +4,233 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
- 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-//import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
 
-	TextView textlog;//Log for outputs
-	 
-    Button buttonConnect;//(dis)connect Button
-    SeekBar seekBar1;//Seekbar to control the Servo
-    SeekBar seekBar2;//Seekbar to control the Servo
-    SeekBar seekBar3;//Seekbar to control the Servo
-    TextView seekBarValue;//Textfield displaing the Value of the seekbar
- 
-    Boolean connected=false;//stores the connection status
- 
-    DataOutputStream dataOutputStream = null;//outputstream to send commands
-    Socket socket = null;//the socket for the connection
-    int kalli=0;
-      
-    
-    // *******************
-    // ** Called when the activity is first created.
-    // *******************
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
- 
-        //connect the view and the objects
-        buttonConnect = (Button)findViewById(R.id.button1);
-        textlog = (TextView)findViewById(R.id.textView3);
-        seekBar1 = (SeekBar)findViewById(R.id.seekBar1);
-        seekBar2 = (SeekBar)findViewById(R.id.seekBar2);
-        seekBar3 = (SeekBar)findViewById(R.id.seekBar3);
-        //seekBarValue = (TextView)findViewById(R.id.seekbarvalue);
- 
-        textlog.setText("Starting Client"+'\n');//log that the App launched
-        changeConnectionStatus(false);//change connectionstatus to "disconnected"
-        
-        //((ScrollView) findViewById(R.id.scrollView1)).fullScroll(View.FOCUS_DOWN);
- 
-        //Eventlisteners
-        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
-        seekBar1.setOnSeekBarChangeListener(seekbarchangedListener1);
-        seekBar2.setOnSeekBarChangeListener(seekbarchangedListener2);
-        seekBar3.setOnSeekBarChangeListener(seekbarchangedListener3);
-    }
-    
-    
-    
-    // *******************
-    // ** SEEKBAR 1 EVENTLISTENER
-    // *******************
-    
-    SeekBar.OnSeekBarChangeListener seekbarchangedListener1 = new SeekBar.OnSeekBarChangeListener(){
-        //Methd is fired everytime the seekbar is changed
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
-            //seekBarValue.setText(valueOfseekbar);//update the value in the textfield
-            
-            outputText("set1:"+valueOfseekbar);
-            
-             if(connected){//if the socket is connected
-                try {
-                     //send a string to the Netduino Server in the form of "set: -seekbarvalue- \n"
-                     dataOutputStream.writeBytes("set:"+valueOfseekbar+'\n');
-            
-                     
-                 }catch (UnknownHostException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 } catch (IOException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 }
-             }
-        }
- 
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-        }
- 
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-        }
-    };
-    
-    
- // *******************
-    // ** SEEKBAR 2 EVENTLISTENER
-    // *******************
-    
-    SeekBar.OnSeekBarChangeListener seekbarchangedListener2 = new SeekBar.OnSeekBarChangeListener(){
-        //Methd is fired everytime the seekbar is changed
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
-            //seekBarValue.setText(valueOfseekbar);//update the value in the textfield
-            
-            outputText("set2:"+valueOfseekbar);
-            
-             if(connected){//if the socket is connected
-                 try {
-                     //send a string to the Netduino Server in the form of "set: -seekbarvalue- \n"
-                     dataOutputStream.writeBytes("set:"+valueOfseekbar+'\n');
-            
-                 }catch (UnknownHostException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 } catch (IOException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 }
-             }
-        }
- 
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-        }
- 
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-        }
-    };
-    
-    
- // *******************
-    // ** SEEKBAR 3 EVENTLISTENER
-    // *******************
-    
-    SeekBar.OnSeekBarChangeListener seekbarchangedListener3 = new SeekBar.OnSeekBarChangeListener(){
-        //Methd is fired everytime the seekbar is changed
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
-            //seekBarValue.setText(valueOfseekbar);//update the value in the textfield
-            
-            outputText("set3:"+valueOfseekbar);
- 
-            if(connected){//if the socket is connected
-                 try {
-                     //send a string to the Netduino Server in the form of "set: -seekbarvalue- \n"
-                     dataOutputStream.writeBytes("set:"+valueOfseekbar+'\n');
-            
-            	
-                 }catch (UnknownHostException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 } catch (IOException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                 }
-             }
-        }
- 
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-        }
- 
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-        }
-    };
-    
-    
-    
-    // *******************
-    // ** CONNECTION BUTTON EVENTLISTENER
-    // *******************
-    
-    Button.OnClickListener buttonConnectOnClickListener = new Button.OnClickListener(){
-        @Override
-        public void onClick(View arg0) {
-            if(!connected){//if not connected yet
-                outputText("connecting to Server");
-                 try {//try to create a socket and outputstream
-                	 outputText("kallig");
-                      socket = new Socket("192.168.156.157", 8000);//create a socket //was port 8888
-                      outputText("skarpig");
-                      dataOutputStream = new DataOutputStream(socket.getOutputStream());//and stream
-                      
-                      outputText("successfully connected");//output the connection status
-                      changeConnectionStatus(true);//change the connection status
-                 } catch (UnknownHostException e) {//catch and
-                	 outputText("unknown host exc");
-                      outputText(e.getMessage());//display errors
-                      
-                      changeConnectionStatus(false);
-                 } catch (IOException e) {//catch and
-                     outputText(e.getMessage());//display errors
-                     outputText("IOdavid exc");
-                     changeConnectionStatus(false);
-                     
-                 }
-            }else{
-                outputText("disconnecting from Server...");
-                try {//try to close the socket
-                      socket.close();
-                      outputText("successfully disconnected");
-                      changeConnectionStatus(false);//change the connection status
-                 } catch (UnknownHostException e) {//catch and
-                	 outputText("unknown host exc2");
-                      outputText(e.getMessage());//display errors
-                 } catch (IOException e) {//catch and
-                	 outputText("IO exc");
-                      outputText(e.getMessage());//display errors
-                 }
-            }
-    }};
-    
-    
-    
-    // *******************
-    // **  Method changes the connection status
-    // *******************
-    
-    public void changeConnectionStatus(Boolean isConnected) {
-        connected=isConnected;//change variable
-        seekBar1.setEnabled(isConnected);//enable/disable seekbar1
-        seekBar2.setEnabled(isConnected);//enable/disable seekbar2
-        seekBar3.setEnabled(isConnected);//enable/disable seekbar3
-        if(isConnected){//if connection established
-            buttonConnect.setText("disconnect");//change Buttontext
-        }else{
-            buttonConnect.setText("connect");//change Buttontext
-        }
-    }
-    
-    
- 
-    // *******************
-    // **  Method appends text to the textfield and adds a newline character
-    // *******************
- 
-    public void outputText(String msg) {
-        textlog.append(msg+"\n");
-    }
+	
+		TextView textlog;//Log for outputs
+		
+		Button buttonConnect;//(dis)connect Button
+		SeekBar seekBar;//Seekbar to control the Servo
+		SeekBar seekBar2;//Seekbar to control the Servo
+		SeekBar seekBar3;//Seekbar to control the Servo
+		TextView seekBarValue;//Textfield displaing the Value of the seekbar
+		TextView seekBarValue2;//Textfield displaing the Value of the seekbar
+		TextView seekBarValue3;//Textfield displaing the Value of the seekbar
+		
+		Boolean connected=false;//stores the connectionstatus
+		
+		DataOutputStream dataOutputStream = null;//outputstream to send commands
+		Socket socket = null;//the socket for the connection
+		
+		
+		
+		//-----------------
+		//Called when the activity is first created. 
+		//------------------
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+		    
+	        //connect the view and the objects
+		    buttonConnect = (Button)findViewById(R.id.connect);
+		    textlog = (TextView)findViewById(R.id.textlog);
+		    seekBar = (SeekBar)findViewById(R.id.seekbar);
+		    seekBar2 = (SeekBar)findViewById(R.id.seekBar1);
+		    seekBar3 = (SeekBar)findViewById(R.id.seekBar2);
+		    seekBarValue = (TextView)findViewById(R.id.seekbarvalue);
+		    seekBarValue2 = (TextView)findViewById(R.id.textView2);
+		    seekBarValue3 = (TextView)findViewById(R.id.textView4);
+		    
+		    textlog.setText("Starting Client");//log that the App launched
+		    changeConnectionStatus(false);//change connectionstatus to "disconnected"
+		    
+		    //Eventlisteners
+		    buttonConnect.setOnClickListener(buttonConnectOnClickListener);
+		    seekBar.setOnSeekBarChangeListener(seekbarchangedListener);
+		    seekBar2.setOnSeekBarChangeListener(seekbarchangedListener2);
+		    seekBar3.setOnSeekBarChangeListener(seekbarchangedListener3);
+	    }
+	    
+	    
+	    
+	    // ----------------------- 
+	    //SEEKBAR EVENTLISTENER - begin 
+	    //----------------------------
+	    SeekBar.OnSeekBarChangeListener seekbarchangedListener = new SeekBar.OnSeekBarChangeListener(){
+	    	//Methd is fired everytime the seekbar is changed
+	    	@Override
+	 	   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+	    		String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
+	    		seekBarValue.setText(valueOfseekbar);//update the value in the textfield
+	    		
+		 	    if(connected){//if the socket is connected
+		 	    	try {
+		 	    		//send a string to the Arduino Server in the form of "set: -seekbarvalue- \n"
+		 	    		dataOutputStream.writeBytes("A"+valueOfseekbar+'\n');
+		 	    	}catch (UnknownHostException e) {//catch and
+		 	    		outputText(e.getMessage());//display errors
+		 			} catch (IOException e) {//catch and
+		 				outputText(e.getMessage());//display errors
+		 			}
+		 	    }
+	 	   }
 
-}
+	 	   @Override
+	 	   public void onStartTrackingTouch(SeekBar seekBar) {
+	 	   }
+
+	 	   @Override
+	 	   public void onStopTrackingTouch(SeekBar seekBar) {
+	 	   }
+	    };
+	    
+	    
+	 // ----------------------- 
+	    //SEEKBAR EVENTLISTENER - begin 
+	    //----------------------------
+	    SeekBar.OnSeekBarChangeListener seekbarchangedListener2 = new SeekBar.OnSeekBarChangeListener(){
+	    	//Methd is fired everytime the seekbar is changed
+	    	@Override
+	 	   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+	    		String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
+	    		seekBarValue2.setText(valueOfseekbar);//update the value in the textfield
+	    		
+		 	    if(connected){//if the socket is connected
+		 	    	try {
+		 	    		//send a string to the Arduino Server in the form of "set: -seekbarvalue- \n"
+		 	    		dataOutputStream.writeBytes("B"+valueOfseekbar+'\n');
+		 	    	}catch (UnknownHostException e) {//catch and
+		 	    		outputText(e.getMessage());//display errors
+		 			} catch (IOException e) {//catch and
+		 				outputText(e.getMessage());//display errors
+		 			}
+		 	    }
+	 	   }
+
+	 	   @Override
+	 	   public void onStartTrackingTouch(SeekBar seekBar) {
+	 	   }
+
+	 	   @Override
+	 	   public void onStopTrackingTouch(SeekBar seekBar) {
+	 	   }
+	    };
+	    
+	    
+	    
+	 // ----------------------- 
+	    //SEEKBAR EVENTLISTENER - begin 
+	    //----------------------------
+	    SeekBar.OnSeekBarChangeListener seekbarchangedListener3 = new SeekBar.OnSeekBarChangeListener(){
+	    	//Methd is fired everytime the seekbar is changed
+	    	@Override
+	 	   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+	    		String valueOfseekbar = String.valueOf(progress);//save the value of the seekbar in a string
+	    		seekBarValue3.setText(valueOfseekbar);//update the value in the textfield
+	    		
+		 	    if(connected){//if the socket is connected
+		 	    	try {
+		 	    		//send a string to the Arduino Server in the form of "set: -seekbarvalue- \n"
+		 	    		dataOutputStream.writeBytes("C"+valueOfseekbar+'\n');
+		 	    	}catch (UnknownHostException e) {//catch and
+		 	    		outputText(e.getMessage());//display errors
+		 			} catch (IOException e) {//catch and
+		 				outputText(e.getMessage());//display errors
+		 			}
+		 	    }
+	 	   }
+
+	 	   @Override
+	 	   public void onStartTrackingTouch(SeekBar seekBar) {
+	 	   }
+
+	 	   @Override
+	 	   public void onStopTrackingTouch(SeekBar seekBar) {
+	 	   }
+	    };
+	    
+	    
+	    // ----------------------- 
+	    //CONNECTION BUTTON EVENTLISTENER - begin 
+	    //----------------------------
+	    Button.OnClickListener buttonConnectOnClickListener = new Button.OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				if(!connected){//if not connected yet
+					outputText("connecting to Server");
+					 try {//try to create a socket and outputstream
+						  socket = new Socket("192.168.157.120", 8000);//create a socket
+						  dataOutputStream = new DataOutputStream(socket.getOutputStream());//and stream
+						  outputText("successfully connected");//output the connection status
+						  changeConnectionStatus(true);//change the connection status
+					 } catch (UnknownHostException e) {//catch and
+						  outputText(e.getMessage());//display errors
+						  changeConnectionStatus(false);
+					 } catch (IOException e) {//catch and
+						 outputText(e.getMessage());//display errors
+						 changeConnectionStatus(false);
+					 }
+				}else{
+					outputText("disconnecting from Server...");
+					try {//try to close the socket
+						  socket.close();
+						  outputText("successfully disconnected");
+						  changeConnectionStatus(false);//change the connection status
+					 } catch (UnknownHostException e) {//catch and
+						  outputText(e.getMessage());//display errors
+					 } catch (IOException e) {//catch and
+						  outputText(e.getMessage());//display errors
+					 }
+				}
+		}};
+		
+		
+		
+		//------------------------
+		// Method changes the connection status
+		//------------------------
+		public void changeConnectionStatus(Boolean isConnected) {
+			connected=isConnected;//change variable
+			seekBar.setEnabled(isConnected);//enable/disable seekbar
+			seekBar2.setEnabled(isConnected);//enable/disable seekbar
+			seekBar3.setEnabled(isConnected);//enable/disable seekbar
+			if(isConnected){//if connection established
+				buttonConnect.setText("disconnect");//change Buttontext
+			}else{
+				buttonConnect.setText("connect");//change Buttontext
+			}
+		}
+		
+		
+		//------------------------
+		//Method appends text to the textfield and adds a newline character
+		//------------------------
+		public void outputText(String msg) {
+			textlog.append(msg+"\n");
+		}
+		
+		
+		
+		//------------------------
+		//Called when the activity is destroied. 
+		//------------------------
+		@Override
+	    public void onDestroy() {
+			if(connected)
+				try {
+					socket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
+	    }
+	}
